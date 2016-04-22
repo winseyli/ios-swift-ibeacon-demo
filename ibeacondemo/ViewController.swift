9d06dc94bd28c2ac7ddb9f95d929f74dbac8f917
Winsey Li <winsey.li@gmail.com>
2016-04-22 11:10:52 +0800

    Initial Commit

---
+++ ibeacondemo/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  ibeacondemo
+//
+//  Created by Winsey Li on 22/4/2016.
+//  Copyright © 2016 codefrd. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
